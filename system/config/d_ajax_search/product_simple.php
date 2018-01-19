59d5c906a358776db2ed12b0b93a5b76d388b516
Dmitriy Sokol <php.drvn@gmail.com>
2018-01-19 11:52:40 +0200

    fast product_simple search
    fast product_simple search

--- system/config/d_ajax_search/product_simple.php
+++ system/config/d_ajax_search/product_simple.php
@@ -13,42 +13,42 @@ $_['d_ajax_search_product_simple'] = array(
             'key' => 'product_id',
             'join' => 'LEFT JOIN',
             'multi_language' => 1
-             ),
-        array(
-            'name' => 'pc',
-            'join_to' => 'p',
-            'full_name' => 'product_to_category',
-            'key' => 'product_id',
-            'join' => 'LEFT JOIN',
-            'multi_language' => 1
+             )
+        // array(
+        //     'name' => 'pc',
+        //     'join_to' => 'p',
+        //     'full_name' => 'product_to_category',
+        //     'key' => 'product_id',
+        //     'join' => 'LEFT JOIN',
+        //     'multi_language' => 1
 
-        ),
-        array(
-            'name' => 'pa',
-            'join_to' => 'p',
-            'full_name' => 'product_attribute',
-            'key' => 'product_id',
-            'join' => 'LEFT JOIN',
-            'multi_language' => 1
+        // ),
+        // array(
+        //     'name' => 'pa',
+        //     'join_to' => 'p',
+        //     'full_name' => 'product_attribute',
+        //     'key' => 'product_id',
+        //     'join' => 'LEFT JOIN',
+        //     'multi_language' => 1
 
-        ),
-        array(
-            'name' => 'cd',
-            'join_to' => 'pc',
-            'full_name' => 'category_description',
-            'key' => 'category_id',
-            'join' => 'LEFT JOIN',
-            'multi_language' => 1
-        ),
-        array(
-            'name' => 'm',
-            'join_to' => 'p',
-            'full_name' => 'manufacturer',
-            'key' => 'manufacturer_id',
-            'join' => 'LEFT JOIN',
-            'multi_language' => 1
+        // ),
+        // array(
+        //     'name' => 'cd',
+        //     'join_to' => 'pc',
+        //     'full_name' => 'category_description',
+        //     'key' => 'category_id',
+        //     'join' => 'LEFT JOIN',
+        //     'multi_language' => 1
+        // ),
+        // array(
+        //     'name' => 'm',
+        //     'join_to' => 'p',
+        //     'full_name' => 'manufacturer',
+        //     'key' => 'manufacturer_id',
+        //     'join' => 'LEFT JOIN',
+        //     'multi_language' => 1
 
-        ),
+        // ),
     ),
     'query' => array(
 
