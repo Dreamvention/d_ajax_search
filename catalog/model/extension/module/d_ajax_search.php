a55088cfbb4c7e9809c4f12276af7e2ba1f4e505
Dmitriy Sokol <php.drvn@gmail.com>
2018-02-05 18:07:09 +0200

    try autocomplite (now works only mobile)

--- catalog/model/extension/module/d_ajax_search.php
+++ catalog/model/extension/module/d_ajax_search.php
@@ -20,24 +20,51 @@ class ModelExtensionModuleDAjaxSearch extends Model {
         $sql_array = array();
         $sql       = '';
 
-        if($research){
-            $sql_smart="SELECT * FROM `" . DB_PREFIX . "as_query` ORDER BY count DESC LIMIT 15";
-            $query=$this->db->query($sql_smart);
-            $gml=0;
-            $new_text='';
+        if($research != 1){
+            $autocomplite_flag=0;
+            $sql_autocomplite="SELECT text FROM `" . DB_PREFIX . "as_query` ORDER BY count DESC LIMIT 100";
+            $query=$this->db->query($sql_autocomplite);
+            $autocomplite=array();
             foreach ($query->rows as $key => $row) {
+
                 similar_text( $text , $row['text'], $percent);
 
-                if($percent > $gml && $percent > 65){
-                    $new_text=$row['text'];
-                    $gml=$percent;
-                }
-            }
-
-            if(!empty($new_text)){
-                $text=$new_text;
+                if($percent > $autocomplite_flag && $percent > 65){
+                        $autocomplite=$row['text'];
+                        $autocomplite_flag=$percent;
+                    }
             }
         }
+
+
+
+        // if($research){
+        //     $sql_smart="SELECT text FROM `" . DB_PREFIX . "as_query` ORDER BY count DESC LIMIT 100";
+        //     $query=$this->db->query($sql_smart);
+        //     $gml=0;
+        //     $lev=20;
+        //     $new_text='';
+        //     foreach ($query->rows as $key => $row) {
+
+        //         // LEVENSTEIN
+        //         // if(levenshtein($text, $row['text']) < $lev){
+        //         //     $new_text=$row['text'];
+        //         //     $lev=levenshtein($text, $row['text']);
+
+        //         // }
+        //         // Oliver's algoritm
+
+        //         similar_text( $text , $row['text'], $percent);
+        //         if($percent > $gml && $percent > 65){
+        //             $new_text=$row['text'];
+        //             $gml=$percent;
+        //         }
+        //     }
+
+        //     if(!empty($new_text)){
+        //         $text=$new_text;
+        //     }
+        // }
 
 
         foreach ($search_filter as $search => $filter) {
@@ -115,6 +142,7 @@ class ModelExtensionModuleDAjaxSearch extends Model {
                     $product_ides[$search][]                = $row[$search . '_id'];
                     $result[$search][$key][$search . '_id'] = $row[$search . '_id'];
                     $result[$search][$key]['keyword'] = $text;
+                    $result[$search][$key]['autocomplite'] = $autocomplite;
                     $result[$search][$key]['image']         = isset($row['image']) ? $this->model_tool_image->resize($row['image'], $settings['image_width'], $settings['image_width']) : '';
                     $result[$search][$key]['name']          = $row['name'];
                     $result[$search][$key]['description']   = isset($row['description']) ? $row['description'] : '';
@@ -157,12 +185,12 @@ class ModelExtensionModuleDAjaxSearch extends Model {
             }
         }
 
-        if($research==0){
-            if(empty($result)){
-                $result=$this->search($text, $searches, $research=1);
-                return $result;
-            }
-        }
+        // if($research==0){
+        //     if(empty($result)){
+        //         $result=$this->search($text, $searches, $research=1);
+        //         return $result;
+        //     }
+        // }
 
         // echo "<pre>"; print_r($result); echo "</pre>";
         $resultOut = array();
