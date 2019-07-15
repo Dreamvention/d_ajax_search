19aac231e313eca1adbcb4c113c52b2bc6d2fe21
Stanislav Shiptya <php5.drvn@gmail.com>
2019-07-15 14:26:31 +0300

    ** fix bug with 2 search keywords.

--- catalog/model/extension/module/d_ajax_search.php
+++ catalog/model/extension/module/d_ajax_search.php
@@ -99,7 +99,7 @@ class ModelExtensionModuleDAjaxSearch extends Model {
 
                     if ($query['rule'] == 'LIKE') {
 
-                        if($query['key']=='pd.name' && $research){
+                        if($query['key']=='pd.name'){
                             foreach ($keywords as $key => $word) {
                                 $implode[$search][] = "LOWER(".$query['key'] . ") LIKE LOWER('%" . $word . "%')";
                             }
